8d317d70addbf1829796668d53f90840aff65cf9
Martazanov <saidmartazanov1@gmail.com>
2023-03-25 23:38:09 +0300

    minimal server

--- models/models.js
+++ models/models.js
@@ -8,13 +8,13 @@ const User = sequelize.define("user", {
   password: { type: DataTypes.STRING },
   role: { type: DataTypes.STRING, defaultValue: "USER" }
 })
-const Basket = sequelize.define("basket", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-})
-const BasketDevice = sequelize.define("BasketDevice", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+// const Basket = sequelize.define("basket", {
+//   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+// })
+// const BasketDevice = sequelize.define("BasketDevice", {
+//   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 
-})
+// })
 // const User = sequelize.define("user", {
 //   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 //   email: { type: DataTypes.STRING, unique: true }
